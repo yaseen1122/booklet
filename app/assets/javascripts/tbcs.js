fbb929196642a2babef571d802f58f7d87246ba9
yaseen <Iqbal>
2020-01-10 02:32:33 +0500

    partially done with putt into booklet flow

--- app/assets/javascripts/tbcs.js
+++ app/assets/javascripts/tbcs.js
@@ -15,5 +15,31 @@ $(document).ready(function(){
   $(" body #add-tbc-btn").on("click",function(){
     $("#new-tbc-form-modal").modal("show");
     $("#new-tbc-form")[0].reset();
+  });
+
+  $('body input.tbcs-check-box').on('change', function() {
+    $('input.tbcs-check-box').not(this).prop('checked', false);
+    $(".tbc-booklrt-tables").hide();
+    let tbcId = $(this).attr("id");
+    $("#place-booklet-fl-btn").data("current-selected-tbc-table",tbcId);
+    $("#"+tbcId+"-booklet-table").show();
+  });
+
+  $('body  input.tbcs-file-check-box').on('change', function() {
+    var $this = $(this);
+    if ($this.is(":checked")) {
+        $this.closest("tr").addClass("highlight");
+    } else {
+        $this.closest("tr").removeClass("highlight");
+    }
+  });
+
+  $("body #place-booklet-fl-btn").on("click",function(){
+    let tableHighlightedRows = $("table.tbc-selected-files-tb tr.highlight")
+    let currentSelectedTbcTableId = $("#place-booklet-fl-btn").data("current-selected-tbc-table");
+    var i;
+    for (i = 0; i < tableHighlightedRows.length; i++) {
+      $("#"+currentSelectedTbcTableId+"-booklet-table").append(tableHighlightedRows[i]);
+    }
   })
 });
